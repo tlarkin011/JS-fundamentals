c1aee4fbef2dfb5031c44cff7e6dc4683879920d
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2022-06-18 11:06:09 -0700

    new stuff

---
+++ Week 3 code.js/.js
@@ -0,0 +1,10 @@
+var value = [3, 9, 23, 64, 2, 8, 28, 93]
+function difference(value){
+const [first] = value;
+console.log(value);
+var reverse = value.reverse();
+console.log(value)
+const [last] = reverse;
+return last - first;
+}
+console.log(difference(value))
